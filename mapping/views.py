698fe0acf65cf827989c4c4cf6696cce6e0545fc
shogo <0814.sho.mura@gmail.com>
2025-07-13 06:53:51 +0900

    においマップとformを表示するビューを作成

--- mapping/views.py
+++ mapping/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
-def init(request):
+
+
+from .models import Smell
+from .forms import SmellForm
+
+
+def smell_map(request):
     """
-    Render the initial page of the mapping application.
+    においマップを表示し、データ登録フォームも表示するビュー。
     """
-    return render(request, 'mapping/init.html')
+    if request.method == 'POST':
+        form = SmellForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = SmellForm()
+    smells = Smell.objects.all()
+    smell_data = [
+        {
+            'name': s.name,
+            'description': s.description,
+            'lat': s.latitude,
+            'lng': s.longitude,
+            'intensity': s.intensity
+        } for s in smells
+    ]
+    import json
+    return render(request, 'mapping/smell_map.html', {
+        'smell_data': json.dumps(smell_data, ensure_ascii=False),
+        'form': form
+    })
 
