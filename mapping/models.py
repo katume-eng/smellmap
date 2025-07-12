82f8d34669a5bc779bf5f0885173176ee457cfae
shogo <0814.sho.mura@gmail.com>
2025-07-13 06:53:21 +0900

    モデルの作成

--- mapping/models.py
+++ mapping/models.py
@@ -19,7 +19,11 @@ class Smell(models.Model):
     """
     name = models.CharField(max_length=100)
     description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    intensity = models.IntegerField(default=1)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='smells')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.latitude}, {self.longitude})"
